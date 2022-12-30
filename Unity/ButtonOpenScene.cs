e327856b7ec0a53062e0ca9b00b7fb14dfee7d10
AlgoritmikaAlmaty <78143317+AlgoritmikaAlmaty@users.noreply.github.com>
2022-12-30 10:50:06 +0600

    Create ButtonOpenScene.cs

---
+++ Unity/ButtonOpenScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOpenScene : MonoBehaviour
+{
+    //Скрипт для открытия новой сцены по нажатию кнопки
+
+    public int sceneId;
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+}
