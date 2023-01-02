2ac3977374f895604918a25f48c9220ffa1bba4a
AlgoritmikaAlmaty <78143317+AlgoritmikaAlmaty@users.noreply.github.com>
2023-01-02 14:20:24 +0600

    Скрипт удаления при касании

---
+++ Unity/Puzzle/Kill.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+
+    //Стандартный скрипт удаления объектов
+    //Можете добавить проверку на тег
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
